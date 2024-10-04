5ce9c5f52f717c6a53d0b5bb8426e7fb25c9d371
mikii <108529128+mikii1i333@users.noreply.github.com>
2024-10-04 20:02:12 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'HISTORIA',
+    'Em que ano ocorreu a Independência do Brasil?',
+    '1822'
+)
+
+criaCartao(
+    'HISTORIA',
+    'Qual evento marcou o início da Segunda Guerra Mundial?',
+    'A invasão da Polônia pela Alemanha'
+)
+
+criaCartao(
+    'LINGUA INGLESA',
+    'Qual significado da gíria tbh?',
+    'Para ser honesto'
+)
+
+criaCartao(
+    'LINGUA INGLESA',
+    'Qual o signiicado da gíria fr?',
+    'Significa afirmar algo verdadeiro'
+)
